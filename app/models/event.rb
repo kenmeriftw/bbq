a304a7b7a1f86e9f3603f1aae15ec40fbca34223
knmrftw <2669997@gmail.com>
2021-04-14 22:01:15 +0300

    bootstrap added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  # Событие принадлежит юзеру
+  belongs_to :user
+  
   # Валидируем заголовок, он не может быть длиннее 255 букв
   validates :title, presence: true, length: {maximum: 255}
   # У события должны быть заполнены место и время
