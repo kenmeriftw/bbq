3b03f20686746c3b0ebd56b126ef12b05152e914
knmrftw <2669997@gmail.com>
2021-04-30 22:44:59 +0300

    gems updated & aws region changed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:               'AWS',
-      region:                 Fog::AWS::Storage::DEFAULT_REGION,
+      region:                 'us-east-2',
       aws_access_key_id:      ENV['S3_ACCESS_KEY'],
       aws_secret_access_key:  ENV['S3_SECRET_KEY']
     }
