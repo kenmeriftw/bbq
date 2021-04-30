bf2c85bd082018d0fd3815ed315aa864c123ed5c
knmrftw <2669997@gmail.com>
2021-04-30 23:55:14 +0300

    locales upgraded

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,7 +38,7 @@ class CommentsController < ApplicationController
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq.reject { |i| i == comment.user.email }
 
     all_emails.each do |mail|
-      EventMailer.comment(event, comment, mail).deliver_now
+      EventMailer.comment(event, comment, mail).deliver_later
     end
   end
 
