c17eb15837fe319e130c1a48cd1ffff80cd08694
Boris Parusnikov <2669997@gmail.com>
2021-05-26 12:57:46 +0300

    lets try

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,10 +6,10 @@ if Rails.env.production?
     config.fog_credentials = {
       provider:               'AWS',
       region:                 'us-east-2',
-      aws_access_key_id:      Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key:  Rails.application.credentials.aws[:secret_access_key]
+      # aws_access_key_id:      Rails.application.credentials.aws[:access_key_id],
+      # aws_secret_access_key:  Rails.application.credentials.aws[:secret_access_key]
     }
 
-    config.fog_directory  = Rails.application.credentials.aws[:bucket_name]
+    # config.fog_directory  = Rails.application.credentials.aws[:bucket_name]
   end
 end
