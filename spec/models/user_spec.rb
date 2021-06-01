5e64fe7cd05eb624e96ac00feca2b727abdaa2e1
Boris Parusnikov <2669997@gmail.com>
2021-06-01 17:58:08 +0300

    omniauth-vkontakte specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,4 +40,44 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe '.find_for_vkontakte_oauth' do
+    let(:access_token) do
+      double(
+        :access_token,
+        provider: 'vkontakte',
+        info: double(email: 'boriska000@gmail.com'),
+        extra: double(raw_info: double(id: '1703636318'))
+            )
+    end
+    context 'when user is not found' do
+      it 'returns newly created user' do
+        user = User.find_for_vkontakte_oauth(access_token)
+
+        expect(user).to be_persisted
+        expect(user.email).to eq 'boriska000@gmail.com'
+      end
+    end
+
+    context 'when user is found via email' do
+      let!(:existing_user) { create(:user, email: 'boriska000@gmail.com') }
+      let!(:some_other_user) { create(:user) }
+
+      it 'returns the user' do
+        expect(User.find_for_vkontakte_oauth(access_token)).to eq existing_user
+      end
+    end
+
+    context 'when user is found via provider&social_url' do
+      let!(:existing_user) do
+        create(:user, provider: 'vkontakte',
+          social_url: 'https://vk.com/1703636318')
+      end
+      let!(:some_other_user) { create(:user) }
+
+      it 'returns the user' do
+        expect(User.find_for_vkontakte_oauth(access_token)).to eq existing_user
+      end
+    end
+  end
 end
