33a7424eff959e768ec6c9375ccda35c13c59f07
knmrftw <2669997@gmail.com>
2021-06-01 23:07:57 +0300

    omniauth_controller refactored

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,14 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def facebook
+    omniauth_provider
+  end
+
+  def vkontakte
+    omniauth_provider
+  end
+
+  def omniauth_provider
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     oauth_provider = request.env['omniauth.auth'].provider
 
